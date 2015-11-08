789ed99c27b9f8902c211b8a6a326b9b42bc6d4d
CarinaKr <carina.krafft@t-online.de>
2015-11-08 17:28:51 +0100

    labyrinth

--- Labyrinth/labyrinth.js
+++ Labyrinth/labyrinth.js
@@ -5,8 +5,9 @@ for(var i=0;i<16;i++)
 {
 	hatFeld[i]=new Array(16);
 }
-var zXSpieler=55;
-var zYSpieler=303;
+var zXSpieler=1;
+var zYSpieler=7;
+var zErwarteEingabe=false;
 
 
 function init()
@@ -28,10 +29,12 @@ function init()
 	zMainCanvas=document.getElementById('main_canvas');
 	zMainCtx=main_canvas.getContext('2d');
 	
+	document.addEventListener("keypress",tasteGedrueckt,false);
+	
 	for(var i=0;i<16;i++)
 	{for(var j=0;j<16;j++)
 		{
-			hatFeld[i]=new Feld(i*50,j*50);
+			hatFeld[i][j]=new Feld(i*50,j*50);
 		}}
 	resetSpiel();
 	ladeBilder();
@@ -59,12 +62,15 @@ function ladeSpielfeld()
 	for(var i=0;i<16;i++)
 	{for(var j=0;j<16;j++)
 		{
-			if()
+			if(localStorage.getItem("Feld"+i+j)=="true")
+			{
+				hatFeld[i][j].zAktiv=true;
+			}
 		}}
 
-	zHintergrundCtx.drawImage(zLabyrinthBild,0,50,800,600,0,0,800,600);
+	zHintergrundCtx.drawImage(zLabyrinthBild,0,0,800,600,0,0,800,600);
 	zMainCtx.clearRect(0,0,800,600);
-	zMainCtx.drawImage(zSpieler,0,0,100,100,zXSpieler,zYSpieler,45,45);
+	zMainCtx.drawImage(zSpieler,0,0,100,100,hatFeld[zXSpieler][zYSpieler].zX+5,hatFeld[zXSpieler][zYSpieler].zY+3,45,45);
 }
 
 function Feld(pX,pY)
@@ -76,7 +82,11 @@ function Feld(pX,pY)
 
 function oben()
 {
-	
+	if(zYSpieler>0&&hatFeld[zXSpieler][zYSpieler-1].zAktiv)
+	{
+		zYSpieler--;
+		pruefeFeld(zXSpieler,zYSpieler);
+	}
 }
 
 function unten()
@@ -94,17 +104,84 @@ function rechts()
 	
 }
 
-resetSpiel()
+function pruefeFeld( pX, pY)
+{
+	zMainCtx.clearRect(0,0,800,600);
+	zMainCtx.drawImage(zSpieler,0,0,100,100,hatFeld[zXSpieler][zYSpieler].zX,hatFeld[zXSpieler][zYSpieler].zX,45,45);
+
+	var pSpiel=localStorage.getItem("spielFeld"+pX+pY);
+	if(pSpiel=="Spiel1"||pSpiel=="Spiel2"||pSpiel=="Spiel3"||pSpiel=="Spiel4"||pSpiel=="Spiel5"||
+		pSpiel=="Spiel6"||pSpiel=="Spiel7")
+		{
+			zErwarteEingabe=true;
+		zMainCtx.fillStyle="grey";
+		zMainCtx.font="20px Arial"
+		zMainCtx.textBaseLine='bottom';
+		zMainCtx.fillText("Press Enter",hatFeld[zXSpieler][zYSpieler].zX,hatFeld[zXSpieler][zYSpieler].zX);
+	}
+	else
+	{zErwarteEingabe=false;}
+	
+}
+
+function tasteGedrueckt(e)
+{
+	var pTaste=e.keyCode || e.which;
+	if(zErwarteEingabe&&pTaste==13)
+	{
+		if(localStorage.getItem("spielFeld"+zXSpieler+zYSpieler)=="Spiel1")	//Spiel 1
+		{
+			window.open("../Mastermind/Mastermind.html","_self");
+		}
+		else if(localStorage.getItem("spielFeld"+zXSpieler+zYSpieler)=="Spiel2")	//Spiel  2
+		{
+			
+		}
+		else if(localStorage.getItem("spielFeld"+zXSpieler+zYSpieler)=="Spiel3")	//Spiel  3
+		{
+			
+		}
+		else if(localStorage.getItem("spielFeld"+zXSpieler+zYSpieler)=="Spiel4")	//Spiel  4
+		{
+			
+		}
+		else if(localStorage.getItem("spielFeld"+zXSpieler+zYSpieler)=="Spiel5")	//Spiel  5
+		{
+			
+		}
+		else if(localStorage.getItem("spielFeld"+zXSpieler+zYSpieler)=="Spiel6")	//Spiel  6
+		{
+			
+		}
+		else if(localStorage.getItem("spielFeld"+zXSpieler+zYSpieler)=="Spiel7")	//Spiel  7
+		{
+			
+		}
+	}
+}
+
+function resetSpiel()
 {
 	for(var i=0;i<16;i++)
 	{for(var j=0;j<16;j++)
 		{
 			localStorage.setItem("Feld"+i+j,"false");
+			localStorage.setItem("spielFeld"+i+j,"");
 		}}
-	localStorage.setItem("Feld"+01+07,"true");
-	localStorage.setItem("Feld"+01+08,"true");
-	for(var k=2;k<=8;k++)
-	{localStorage.setItem("Feld"+02+0k,"true");}
+	localStorage.setItem("Feld"+1+7,"true");
+	localStorage.setItem("Feld"+1+8,"true");
+	localStorage.setItem("Feld"+1+9,"true");
+	for(var k=2;k<=10;k++)
+	{localStorage.setItem("Feld"+2+k,"true");}
+	localStorage.setItem("Feld"+3+2,"true");
+	
+	localStorage.setItem("spielFeld"+1+9,"Spiel1");
+	localStorage.setItem("spielFeld"+3+2,"Spiel2");
+	localStorage.setItem("spielFeld"+4+6,"Spiel3");
+	localStorage.setItem("spielFeld"+6+3,"Spiel4");
+	localStorage.setItem("spielFeld"+3+8,"Spiel5");
+	localStorage.setItem("spielFeld"+9+4,"Spiel6");
+	localStorage.setItem("spielFeld"+13+4,"Spiel7");
 }
 
 function backToTheMaze()
