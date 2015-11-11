dd41b292dfda59a0733b87696f9a12402f25bf26
CarinaKr <carina.krafft@t-online.de>
2015-11-11 15:42:38 +0100

    snake

---
+++ Labyrinth/Snake/snake.js
@@ -0,0 +1,287 @@
+var version='0.0.1';
+var zXFelder=42; var zYFelder=32;
+var hatFeld=new Array(zXFelder);
+for(var i=0;i<zXFelder;i++)
+{
+	hatFeld[i]=new Array(zYFelder);
+}
+var hatKopf;
+var hatFutter;
+var hatKoerper=new Array();
+var zGroesse=20;
+var zGewonnen=false;
+var zGameOver=false;
+var zGameStarted=false;
+var zPunkte;
+var zRichtung;
+var zZeitStart=50;
+var zWarteZeit;
+var zIndexKoerper=2;
+
+function init()
+{
+	requestaframe=(function()
+	  { return window.requestAnimationFrame        ||
+			window.webkitRequestAnimationFrame  ||
+			window.mozRequestAnimationFrame     ||
+			window.oRequestAnimationFrame       ||
+			window.msRequestAnimationFrame      ||
+			 function(callback)
+			 { window.setTimeout(callback,1000/60);}	
+		 ;
+	  })();
+	
+			
+	zHintergrundCanvas=document.getElementById('background_canvas');
+	zHintergrundCtx=background_canvas.getContext('2d');
+	zMainCanvas=document.getElementById('main_canvas');
+	zMainCtx=main_canvas.getContext('2d');
+	
+	document.addEventListener("keydown",tasteRunter,false);
+	
+	
+	for(var m=0;m<zXFelder;m++)
+	{for(var n=0;n<zYFelder;n++)
+		{hatFeld[m][n]=new Feld(-zGroesse+m*zGroesse,-zGroesse+n*zGroesse);
+		}
+	}
+
+	for (var i=0;i<zIndexKoerper;i++)
+	{hatKoerper[i]=new Koerper(0,i);}
+	hatKopf=new Kopf(0,2);
+	hatFutter=new Futter(10,10);
+	ladeBilder();
+	resetSpielfeld();
+	loop();
+}
+function mouse(e)
+{
+	zMausX=e.pageX-document.getElementById('game_object').offsetLeft;
+	zMausY=e.pageY-document.getElementById('game_object').offsetTop;
+	document.getElementById('x').innerHTML=zMausX;
+	document.getElementById('y').innerHTML=zMausY;	
+}
+
+function ladeBilder()
+{
+	zBild=new Image();
+	zBild.src='bilder/gesamt.png';
+}
+
+function resetSpielfeld()
+{   
+	zPunkte=0;
+	zRichtung=1;
+	zZeitStart=50;
+	zWarteZeit=zZeitStart;
+	zIndexKoerper=2;
+	zGameStarted=true;
+	hatKopf.zX=1; hatKopf.zY=3;
+	hatKoerper[0].zX=1; hatKoerper[0].zY=2;
+	hatKoerper[1].zX=1; hatKoerper[1].zY=1;
+	hatFutter.zX=Math.round(Math.random()*zXFelder);
+	hatFutter.zY=Math.round(Math.random()*zYFelder);
+	if(hatFutter.zX<1||hatFutter.zX>zXFelder-2)
+	{hatFutter.zX=20;}
+	if(hatFutter.zY<1||hatFutter.zY>zYFelder-2)
+	{hatFutter.zY=20;}
+}
+
+function Feld(pX,pY)
+{
+	this.zX=pX;
+	this.zY=pY;
+}
+
+function Kopf(pX,pY)
+{
+	this.zX=pX;
+	this.zY=pY;
+}
+Kopf.prototype.draw=function()
+{
+	zMainCtx.drawImage(zBild,50,50,50,50,-zGroesse+this.zX*zGroesse,-zGroesse+this.zY*zGroesse,zGroesse,zGroesse);
+};
+
+function Futter(pX,pY)
+{
+	this.zX=pX;
+	this.zY=pY;
+}
+Futter.prototype.draw=function()
+{
+	zMainCtx.drawImage(zBild,50,0,50,50,-zGroesse+this.zX*zGroesse,-zGroesse+this.zY*zGroesse,zGroesse,zGroesse);
+};
+
+function Koerper(pX,pY)
+{
+	this.zX=pX;
+	this.zY=pY;
+}
+Koerper.prototype.draw=function()
+{
+	zMainCtx.drawImage(zBild,0,0,50,50,-zGroesse+this.zX*zGroesse,-zGroesse+this.zY*zGroesse,zGroesse,zGroesse);
+};
+
+function pruefeGameOver()
+{
+	if(zPunkte==10)
+	{
+		zGameOver=true;
+		zGewonnen=true;
+	}
+}
+
+function tasteRunter(e)
+{
+	var key_id=e.keyCode || e.which;
+	if(key_id==40) //down key
+	{zRichtung=2;
+	 e.preventDefault();
+	}
+	if(key_id==38) //up key
+	{zRichtung=4;
+	 e.preventDefault();
+	}
+	if(key_id==37) //left key
+	{zRichtung=3;
+	 e.preventDefault();
+	}
+	if(key_id==39) //right key
+	{zRichtung=1;
+	 e.preventDefault();
+	}
+}
+
+function restart()
+{
+	zGameStarted=false;
+	zGewonnen=false;
+	zGameOver=false;
+	resetSpielfeld();
+}
+
+function loop()
+{
+	//Feld zeu zeichnen
+	zMainCtx.clearRect(0,0,800,600);
+	if(hatKopf!=null&&hatKoerper[0]!=null)
+	{hatKopf.draw();
+	 hatFutter.draw();
+	 for(var i=0;i<zIndexKoerper;i++)
+	 {hatKoerper[i].draw();}
+	}
+	zMainCtx.fillStyle="white";
+		zMainCtx.font="20px Arial"
+		zMainCtx.textBaseLine='top';
+		zMainCtx.fillText("Points: "+zPunkte,20,20);
+	
+	//Schlange bewegen
+	if(!zGameOver&&zWarteZeit==0)
+	{
+		if(zRichtung==1)  //bewege nach rechts
+		{
+			for(var i=zIndexKoerper-1;i>=1;i--)
+			{
+				hatKoerper[i].zX=hatKoerper[i-1].zX;
+				hatKoerper[i].zY=hatKoerper[i-1].zY;
+			}
+			hatKoerper[0].zX=hatKopf.zX;
+			hatKoerper[0].zY=hatKopf.zY;
+			hatKopf.zX++;
+		}
+		else if(zRichtung==2) //bewege nach unten
+		{
+			for(var i=zIndexKoerper-1;i>=1;i--)
+			{
+				hatKoerper[i].zX=hatKoerper[i-1].zX;
+				hatKoerper[i].zY=hatKoerper[i-1].zY;
+			}
+			hatKoerper[0].zX=hatKopf.zX;
+			hatKoerper[0].zY=hatKopf.zY;
+			hatKopf.zY++;
+		}
+		else if(zRichtung==3) //bewege nach link
+		{
+			for(var i=zIndexKoerper-1;i>=1;i--)
+			{
+				hatKoerper[i].zX=hatKoerper[i-1].zX;
+				hatKoerper[i].zY=hatKoerper[i-1].zY;
+			}
+			hatKoerper[0].zX=hatKopf.zX;
+			hatKoerper[0].zY=hatKopf.zY;
+			hatKopf.zX--;
+		}
+		else if(zRichtung==4) //bewege nach oben
+		{
+			for(var i=zIndexKoerper-1;i>=1;i--)
+			{
+				hatKoerper[i].zX=hatKoerper[i-1].zX;
+				hatKoerper[i].zY=hatKoerper[i-1].zY;
+			}
+			hatKoerper[0].zX=hatKopf.zX;
+			hatKoerper[0].zY=hatKopf.zY;
+			hatKopf.zY--;
+		}
+		
+		if(hatKopf.zX==hatFutter.zX&&hatKopf.zY==hatFutter.zY)
+		{
+			zPunkte++;
+			hatFutter.zX=Math.round(Math.random()*zXFelder);
+			hatFutter.zY=Math.round(Math.random()*zYFelder);
+			if(hatFutter.zX<1||hatFutter.zX>zXFelder-2)
+			{hatFutter.zX=20;}
+			if(hatFutter.zY<1||hatFutter.zY>zYFelder-2)
+			{hatFutter.zY=20;}
+			{hatFutter.zY=20;}
+			zZeitStart-=5;
+			zIndexKoerper++;
+			hatKoerper[zIndexKoerper-1]=new Koerper(0,0);
+		}
+		
+		if(hatKopf.zX<1||hatKopf.zX>zXFelder-2||hatKopf.zY<1||hatKopf.zY>zYFelder-2)
+		{
+			zGameOver=true;
+		}
+		for(var j=0;j<zIndexKoerper;j++)
+		{
+			if(hatKopf.zX==hatKoerper[j].zX&&hatKopf.zY==hatKoerper[j].zY)
+			{zGameOver=true;}
+		}
+		zWarteZeit=zZeitStart;
+	}
+	zWarteZeit--;
+	
+	
+	zGewonnen=false;
+	pruefeGameOver();
+	if(zGewonnen&&zGameStarted)
+	{
+		zMainCtx.fillStyle="red";
+		zMainCtx.font="50px Arial"
+		zMainCtx.textBaseLine='top';
+		zMainCtx.fillText("You won!",100,200);
+	}
+	else if(zGameOver)
+	{
+		zMainCtx.fillStyle="red";
+		zMainCtx.font="50px Arial"
+		zMainCtx.textBaseLine='top';
+		zMainCtx.fillText("You lost!",100,200);
+	}
+
+	requestaframe(loop);
+}
+
+function backToTheMaze()
+{
+	if(zGewonnen)
+	{
+		localStorage.setItem("SnakeGewonnen","true");
+	}
+	localStorage.setItem("SnakeGewonnen","true");
+	close();
+}
+
+
+init();
